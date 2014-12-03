91d92424127f0ff5e37996efb223f194fa7d7ecb
Joseph Lord <joseph@human-friendly.com>
2014-12-03 12:01:14 +0000

    Initial Commit

---
+++ AssertingNilCoaelesce/AssertingNilCoaelesce.h
@@ -0,0 +1,19 @@
+//
+//  AssertingNilCoaelesce.h
+//  AssertingNilCoaelesce
+//
+//  Created by Joseph on 03/12/2014.
+//  Copyright (c) 2014 Joseph Lord. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AssertingNilCoaelesce.
+FOUNDATION_EXPORT double AssertingNilCoaelesceVersionNumber;
+
+//! Project version string for AssertingNilCoaelesce.
+FOUNDATION_EXPORT const unsigned char AssertingNilCoaelesceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssertingNilCoaelesce/PublicHeader.h>
+
+
